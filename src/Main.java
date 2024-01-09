d07e764df2fde08e4a6e94b81a5080600d49702f
yuuksibunta <idayuuki.tiu@gmail.com>
2024-01-09 20:37:13 +0900

    PUSHコマンドの確認

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main{
+    public static void main(String[] args){
+      int x = 5;
+      int result = ++x;
+      System.out.println(result);
+      System.out.println(x);
+
+    }
+}
+
