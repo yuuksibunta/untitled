c7b4471dcd583f88fcc35dcdfbb3c93af7bade48
yuuksibunta <idayuuki.tiu@gmail.com>
2024-01-20 11:05:22 +0900

    âコミットの確認

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        int x = 19;
-        x++;
+        int [] scores = new int[3];
+        int num = scores.length;
 
-        System.out.println(x);
-        System.out.println(x/2);
+        System.out.println(0);
+        System.out.println(2);
         System.out.println("練習１回目");
+        System.out.println("練習2回め");
+
+
     }
 }
